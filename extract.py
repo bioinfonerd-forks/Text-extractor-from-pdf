6ddd4640fe4938818a5e997a0b4b3d3c9917380f
Murthy Gidugu <narayanamurthy.gidugu@gmail.com>
2019-06-19 12:49:27 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,18 +1,17 @@
 import pytesseract 
-import sys 
 from pdf2image import convert_from_path 
-import os
-def extract_text_pdf_ocr(pdf_path,out_path):
-    f = open(out_path, "a")  
-    pages = convert_from_path(pdf_path, 500) 
-    for page in pages: 
-       
-        text = str(pytesseract.image_to_string(page))
-        text = text.replace('-\n', '')     
-        f.write(text) 
-    f.close() 
+import time
+from _thread import *
+OMP_THREAD_LIMIT=4
+def extract_text_pdf_ocr(pdf_path):
+        text=''
+        pages = convert_from_path(pdf_path, 100) 
+        for page in pages: 
+                text += str(pytesseract.image_to_string(page))
+        return text
 if __name__ == '__main__':
-    pdf_path=input("Enter name of pdf file:")
-    out_path=pdf_path.split('.')[0]
-    extract_text_pdf_ocr(pdf_path,out_path)
-   
+    start=time.time()
+    text=extract_text_pdf_ocr('Arpita Bose cv.pdf') 
+    end=time.time()
+    print(end-start,'\n',text)
+    
